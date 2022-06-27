7aba06fa16d59d98790a64266b5b22178e4f9ff3
Kai <khir829@aucklanduni.ac.nz>
2022-06-27 15:56:46 +1200

    Add user input in generating password

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		boolean flag;
+		int i = 0;
+		String o = null;
+		Scanner scan = new Scanner(System.in);
+		do {
+			do {
+				try {
+					Scanner sc = new Scanner(System.in);
+					System.out.print("Enter a password length that is 4 or more characters: ");
+					i = sc.nextInt();
+					if (i < 4) {
+						System.out.print("Input value is less than 4. ");
+					}
+					flag = false;
+				} catch (Exception e) {
+					System.out.print("Enter only integer values. ");
+					flag = true;
+				}
 
-		Password password = new Password(9, true, true, true);
-		System.out.println(password.getPasswordFinal());
+			} while (flag || i < 4);
+
+			Password password = new Password(i + 1);
+			System.out.println("This is your generated password: " + password.getPasswordFinal());
+			System.out.print("\nPress Enter to generate another password");
+			o = scan.nextLine();
+			System.out.println();
+		} while (o instanceof String);
+
 	}
-
 }
--- src/Password.java
+++ src/Password.java
@@ -9,30 +9,27 @@ public class Password {
 	protected String passwordFinal;
 	StringBuffer sb = new StringBuffer();
 
-	public Password(int length, boolean upper, boolean number, boolean special) {
+	public Password(int length) {
 		String[] password = new String[length];
 
-		if (true) {
+		for (int i = 0; i < length - 1; i += 4) {
+			int posLow = random.nextInt(lowerCaseLetters.length());
+			password[i] = lowerCaseLetters.substring(posLow, posLow + 1);
+		}
 
-			for (int i = 0; i < length - 1; i += 4) {
-				int posLow = random.nextInt(lowerCaseLetters.length());
-				password[i] = lowerCaseLetters.substring(posLow, posLow + 1);
-			}
+		for (int i = 1; i < length - 1; i += 4) {
+			int posUp = random.nextInt(capitalCaseLetters.length());
+			password[i] = capitalCaseLetters.substring(posUp, posUp + 1);
+		}
 
-			for (int i = 1; i < length - 1; i += 4) {
-				int posUp = random.nextInt(capitalCaseLetters.length());
-				password[i] = capitalCaseLetters.substring(posUp, posUp + 1);
-			}
+		for (int i = 2; i < length - 1; i += 4) {
+			int posNumber = random.nextInt(numbers.length());
+			password[i] = numbers.substring(posNumber, posNumber + 1);
+		}
 
-			for (int i = 2; i < length - 1; i += 4) {
-				int posNumber = random.nextInt(numbers.length());
-				password[i] = numbers.substring(posNumber, posNumber + 1);
-			}
-
-			for (int i = 3; i < length - 1; i += 4) {
-				int posSpecial = random.nextInt(specialCharacters.length());
-				password[i] = specialCharacters.substring(posSpecial, posSpecial + 1);
-			}
+		for (int i = 3; i < length - 1; i += 4) {
+			int posSpecial = random.nextInt(specialCharacters.length());
+			password[i] = specialCharacters.substring(posSpecial, posSpecial + 1);
 		}
 
 		for (int i = 0; i < password.length - 1; i++) {
